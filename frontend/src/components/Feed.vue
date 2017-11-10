88aa3348728ac686e70810536547ae31ee3b1f9e
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2017-11-10 15:35:43 -0500

    Trying out another infinite loader

--- frontend/src/components/Feed.vue
+++ frontend/src/components/Feed.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="page-wrapper">
+    <md-button class="md-fab md-primary md-mini md-clean" @click="logData" style="position: fixed; z-index: 9999999999999">
+      <md-icon>bug_report</md-icon>
+    </md-button>
     <md-layout class="feed-container" md-gutter="16">
       <md-layout class="feed-block" md-flex-xsmall="100" md-flex-small="50" md-flex-medium="33" md-flex-large="25" md-flex-xlarge="20" v-for="statement in statements" :key="statement">
         <feed-card :statement="statement"></feed-card>
       </md-layout>
-      <md-button class="md-fab add-button" @click="sendStatement">
+      <!-- <md-button class="md-fab add-button" @click="sendStatement">
         <md-icon>add</md-icon>
-      </md-button>
+      </md-button> -->
+      <md-speed-dial md-mode="scale" class="md-fab add-button">
+        <md-button class="md-fab" md-fab-trigger>
+          <md-icon md-icon-morph>close</md-icon>
+          <md-icon>share</md-icon>
+        </md-button>
+        <md-button class="md-fab md-primary md-mini md-clean" @click="sendStatement">
+          <md-icon>add</md-icon>
+        </md-button>
+        <md-button class="md-fab md-primary md-mini md-clean">
+          <md-icon>refresh</md-icon>
+        </md-button>
+      </md-speed-dial>
     </md-layout>
     <infinite-loading @infinite="getStatements" ref="infiniteLoading" v-if="statements.length >= 20">
       <span slot="spinner">
@@ -20,6 +35,7 @@
 </template>
 
 <script>
+import _ from 'lodash';
 import StatementService from 'src/services/StatementService';
 import EventBus from 'src/buses/EventBus';
 import InfiniteLoading from 'vue-infinite-loading';
@@ -49,8 +65,14 @@ export default {
       }
       return false;
     },
-    getStatements($state) {
-      console.log(2);
+    // getStatements ($state) {
+    //   console.log(1);
+    //   _.debounce(($state) => {
+    //
+    //   }, 2000);
+    // },
+    getStatements: _.debounce(($state) => {
+      console.log(this);
       if (!this.loading) {
         this.loading = true;
         StatementService.getStatements(this.moreUrl, (err, response, body) => {
@@ -81,7 +103,44 @@ export default {
           $state.reset();
         }
       }
-    },
+    }, 2000),
+    // getStatements($state) {
+    //   console.log('Getting statements');
+    //   if (!this.loading) {
+    //     console.log(1);
+    //     this.loading = true;
+    //     StatementService.getStatements(this.moreUrl, (err, response, body) => {
+    //       if (err) {
+    //         console.error(err);
+    //       }
+    //       else {
+    //         console.log(2);
+    //         this.moreUrl = body.more;
+    //         // this.statements.push(...body.statements);
+    //         console.log(3);
+    //         body.statements.forEach((statement) => {
+    //           if (!this.containsObject(statement, this.statements)) {
+    //             this.statements.push(statement);
+    //           }
+    //         });
+    //         console.log(this.statements);
+    //         if (!this.moreUrl || this.moreUrl === '') {
+    //           $state.complete();
+    //         }
+    //       }
+    //       this.loading = false;
+    //       this.initiallyLoaded = true;
+    //       if ($state) {
+    //         $state.loaded();
+    //       }
+    //     })
+    //   }
+    //   else {
+    //     if (!this.moreUrl && $state) {
+    //       $state.reset();
+    //     }
+    //   }
+    // },
     sendStatement() {
       this.loading = true;
       StatementService.buildStatement()
@@ -101,6 +160,9 @@ export default {
         .catch(error => {
           this.loading = false;
         })
+    },
+    logData () {
+      console.log(this.loading);
     }
   },
   mounted () {
@@ -125,19 +187,19 @@ export default {
       background: $darkGray;
     }
   }
-  
+
   .page-wrapper {
     padding: 2rem 2rem 4rem;
     position: relative;
-    
+
     .feed-container {
       margin-right: 0;
       margin-left: 0;
-      
+
       .feed-block {
         margin-bottom: 1rem;
       }
-      
+
       .add-button {
         position: fixed;
         z-index: 1000;
@@ -145,7 +207,7 @@ export default {
         right: 24px;
       }
     }
-    
+
     .loading-screen {
       position: absolute;
       top: 0;
@@ -161,7 +223,7 @@ export default {
       animation: fadeIn 0.2s linear;
     }
   }
-  
+
   // Bug fix for vue-material
   @media (min-width: 601px) and (max-width: 944px) {
     .md-flex-small-50 {
