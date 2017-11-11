42842952933539017a25bfa07580216fb2d3a40c
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2017-11-10 19:54:53 -0500

    Touch ups

--- server.js
+++ server.js
@@ -2,22 +2,14 @@ var express=require('express');
 var path = require('path');
 var app=express();
 var serveStatic = require('serve-static');
-// Allow access to static data
-// app.use('/frontend', express.static('frontend'))
-// app.use('/data', express.static('frontend/data'))
-// app.use('/assets', express.static('frontend/assets'))
-//
-// // Use index page as default route
-// app.get('/',function(req,res){
-//   res.sendFile('/frontend/dist/index.html');
-// });
 
+// Allow access to static data
 app.use(serveStatic(__dirname + "/frontend/dist"));
 
 // Redirect any other routes back to the index page
-// app.get('*', function(req, res){
-//   res.redirect('/');
-// });
+app.get('*', function(req, res){
+  res.redirect('/');
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Express Started!");
